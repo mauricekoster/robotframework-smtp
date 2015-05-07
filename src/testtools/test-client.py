03fd94587c94999f48a49c9c0109bde0f1131b5e
Maurice Koster <maurice.koster@ing.nl>
2015-05-07 12:28:26 +0200

    Testje

--- src/testtools/test-client.py
+++ src/testtools/test-client.py
@@ -11,6 +11,7 @@ msg['Subject'] = 'Simple test message'
 server = smtplib.SMTP('127.0.0.1', 25)
 server.set_debuglevel(True) # show communication with the server
 try:
-    server.sendmail('author@example.com', ['recipient1@example.com', 'recipient2@example.com'], msg.as_string())
+    #server.sendmail('author@example.com', ['recipient1@example.com', 'recipient2@example.com'], msg.as_string())
+    server.sendmail('author@example.com', ['recipient1@yo.com'], msg.as_string())
 finally:
     server.quit()
